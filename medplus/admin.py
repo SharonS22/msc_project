efbfbd82ce8ac4ce52f0cec266833fe9fce5e3fa
Sharon Shaji <2259122s@student.gla.ac.uk>
2022-07-11 19:12:37 +0100

    data added

--- medplus/admin.py
+++ medplus/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from medplus.models import Data 
+
+admin.site.register(Data)
 
 # Register your models here.
--- medplus/models.py
+++ medplus/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Data(models.Model):
+    outbreak = models.CharField(("outbreak"), max_length=100)
+    full_loc = models.CharField(("full location"), max_length=100)
+    place = models.CharField(("place"), max_length=50)
+    continent= models.CharField(("continent"), max_length=100)
+    updates = models.IntegerField(("number of updates"))
+    date = models.DateField(("date"))
+    
+    
